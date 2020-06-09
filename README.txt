cec684a97f0b4cdb04055076299f0f3b4f92cdab
bhavesh <bhaveshvaghela32@gmail.com>
2020-06-09 19:40:47 +0530

    driver potial earning functionality

---
+++ README.txt
@@ -0,0 +1,2 @@
+1 change ApiUrl and APIHasKey in www/js/config.js
+2 Update www/index.html with map key, current I have setup working key. 
---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"http://localhost/ofori/driver/api",		
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : '',
+	'APIHasKey':"myapihashkey",
+	'debug': true
+};
